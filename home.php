ce095ff961037383bfcadd04f07e735fd16fa64a
Ramesh Peshala <rameshpeshala84@gmail.com>
2024-12-02 13:42:55 +0530

    Add initial structure for travel & tour website with home page and styles

---
+++ home.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home/title>
+
+
+        <!-- swiper css link -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
+
+        <!-- font awesome cdn link -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/all.min.css"
+            integrity="sha512-5Hs3dF2AEPkpNAR7UiOHba+lRSJNeM2ECkwxUIxC1Q/FLycGTbNapWXB4tP889k5T5Ju8fs4b1P5z/iB4nMfSQ=="
+            crossorigin="anonymous" referrerpolicy="no-referrer" />">
+
+
+        <!-- custom css file link -->
+        <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+
+    <!-- header section starts  -->
+
+      <section class="header">
+          <a href="home.php" class="logo">travel.</a>
+
+          <nav class="navbar">
+            <a href="home.php">home</a>
+            <a href="about.php">About</a>
+            <a href="package.php">Package</a>
+            <a href="book.php">Book</a>
+          </nav>
+      </section>
+     <!-- header sectio  ends -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <!-- swiper js cdn link -->
+    <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+
+    <!-- custom js file -->
+    <script src="js/script.js"></script>
+
+</body>
+
+</html>
