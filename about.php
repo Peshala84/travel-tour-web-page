465475a27009777378964f80893f9244aee90791
Ramesh Peshala <rameshpeshala84@gmail.com>
2024-12-13 10:24:54 +0530

    Add about section with content, images, and client reviews slider

--- about.php
+++ about.php
@@ -32,9 +32,149 @@
     <div id="menu-btn" class="fas fa-bars"></div>
   </section>
   <!-- header section ends -->
-<div class="heading" style="">
+<div class="heading" style="background:url(./images/footer_bg.jpg)">
     <h1>About Us</h1>
 </div>
+
+<!-- about section starts -->
+
+   <section class="about">
+    <div class="image">
+    <img src="./images/footer_bg.jpg" alt="">
+    </div>
+    <div class="content">
+      <h3>why choose us?</h3>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nemo reiciendis 
+        veritatis repellat necessitatibus, quibusdam consequatur ea quae 
+        cupiditate illo unde?</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing
+           elit. Velit sunt libero provident cumque id.</p>
+           <div class="icons-container">
+
+            <div class="icons">
+              <i class="fas fa-map"></i>
+              <span>top destintions</span>
+            </div>
+
+            <div class="icons">
+              <i class="fas fa-hand-holding-usd"></i>
+              <span>affordable price</span>
+            </div>
+
+            <div class="icons">
+              <i class="fas fa-headset"></i>
+              <span>24/7 guide service</span>
+            </div>
+
+           </div>
+    </div>
+   </section>
+
+<!-- about section ends -->
+
+<!-- review section starts -->
+    <section class="reviews">
+      <h1 class="heading-title">Clients reviews</h1>
+      <div class="swiper reviews-slider">
+        <div class="swiper-wrapper">
+
+          <div class="swiper-slider slide">
+            <div class="stars">
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+            </div>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perspiciatis 
+              itaque corporis error, fugit facere veniam animi aliquid pariatur assumenda 
+              esse modi nemo accusantium architecto debitis, at laboriosam! Delectus, maiores. Suscipit!</p>
+              <h3>Ramesh Peshala</h3>
+              <span>traveler</span>
+              <img src="./images/head_image_01.jpg" alt="">
+          </div>
+
+          <div class=" swiper-slider slide">
+            <div class="stars">
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+            </div>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perspiciatis 
+              itaque corporis error, fugit facere veniam animi aliquid pariatur assumenda 
+              esse modi nemo accusantium architecto debitis, at laboriosam! Delectus, maiores. Suscipit!</p>
+              <h3>Ramesh Peshala</h3>
+              <span>traveler</span>
+              <img src="./images/head_image_01.jpg" alt="">
+          </div>
+
+          <div class="swiper-slider slide">
+            <div class="stars">
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+            </div>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perspiciatis 
+              itaque corporis error, fugit facere veniam animi aliquid pariatur assumenda 
+              esse modi nemo accusantium architecto debitis, at laboriosam! Delectus, maiores. Suscipit!</p>
+              <h3>Ramesh Peshala</h3>
+              <span>traveler</span>
+              <img src="./images/head_image_01.jpg" alt="">
+          </div>
+
+          <div class=" swiper-slider slide">
+            <div class="stars">
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+            </div>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perspiciatis 
+              itaque corporis error, fugit facere veniam animi aliquid pariatur assumenda 
+              esse modi nemo accusantium architecto debitis, at laboriosam! Delectus, maiores. Suscipit!</p>
+              <h3>Ramesh Peshala</h3>
+              <span>traveler</span>
+              <img src="./images/head_image_01.jpg" alt="">
+          </div>
+
+          <div class=" swiper-slider slide">
+            <div class="stars">
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+            </div>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perspiciatis 
+              itaque corporis error, fugit facere veniam animi aliquid pariatur assumenda 
+              esse modi nemo accusantium architecto debitis, at laboriosam! Delectus, maiores. Suscipit!</p>
+              <h3>Ramesh Peshala</h3>
+              <span>traveler</span>
+              <img src="./images/head_image_01.jpg" alt="">
+          </div>
+
+          <div class=" swiper-slider slide">
+            <div class="stars">
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+            </div>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perspiciatis 
+              itaque corporis error, fugit facere veniam animi aliquid pariatur assumenda 
+              esse modi nemo accusantium architecto debitis, at laboriosam! Delectus, maiores. Suscipit!</p>
+              <h3>Ramesh Peshala</h3>
+              <span>traveler</span>
+              <img src="./images/head_image_01.jpg" alt="">
+          </div>
+
+        </div>
+      </div>
+    </section>
+  
+<!-- review section ends -->
 
 
   <section class="footer">
